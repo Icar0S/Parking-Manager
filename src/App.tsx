adb855ef2ad770ba7690660a7d0c6b24907f8a88
icar0S <icarooliveira_23@hotmail.com>
2023-02-14 10:21:14 -0300

    feat: adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter>
+
+      <AppRoutes />
+
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<p>Home</p>} />
+
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  )
+};
