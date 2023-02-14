0962e56f6e7c8ece8f3f0d99a5f746e258cfe42e
icar0S <icarooliveira_23@hotmail.com>
2023-02-14 11:00:08 -0300

    feat: add thema Light

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Button>Hello</Button>} />
+      <Route path="/home" element={<Button variant='contained' color='primary'>Hello</Button>} />
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material";
+import { blue, purple } from '@mui/material/colors';
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+      dark: blue[800],
+      light: blue[500],
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: purple[500],
+      dark: purple[400],
+      light: purple[300],
+      contrastText: '#ffffff',
+    },
+    background: {
+      paper: '#ffffff',
+      default: '#f7f6f3',
+    }
+  }
+
+});
