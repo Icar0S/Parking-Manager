72c1c2ecb13473c3167c711c991a58f64463c004
icar0S <icarooliveira_23@hotmail.com>
2023-03-15 10:21:31 -0300

    feat: configurando variáveis de ambiente

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,20 @@
-export { };
+
+
+export const Enviroment = {
+  /**
+   * Define a quantidade de linhas a ser carregada por padrão na listagem
+   */
+  LIMITE_DE_LINHAS: 10,
+
+  /**
+   * Placeholder no input
+   */
+  INPUT_DE_BUSCA: 'Pesquisar...',
+
+  /**
+   * texto padrão a ser exibido
+   */
+  LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+
+  URL_BASE: 'http://localhost:3333',
+};
--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { Enviroment } from '../../../environment';
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: Enviroment.URL_BASE
 });
 
 Api.interceptors.response.use(
